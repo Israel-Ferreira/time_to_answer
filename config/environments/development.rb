5a57d6aada163433dedc0123edbe8164ce456ecd
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-03-26 22:39:53 -0300

    Adicionando o devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -32,7 +35,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome/index'
   get '/inicio', to: 'welcome#index'
