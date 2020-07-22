a6b0a2466ec57ca5ca568a2808aefc9f107d7a2e
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-07-22 20:30:13 -0300

    Separando o layout de login de admin do user comum

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -4,9 +4,6 @@ class AdminsBackofficeController < ApplicationController
   before_action :authenticate_admin!
   layout 'admins_backoffice'
 
-  def after_sign_in_path_for(_resource)
-    redirect_to admins_backoffice_welcome_index_path
-  end
 
   def back_resource_index(index_route, notice_msg)
     redirect_to index_route, notice: notice_msg
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && resource_class == 'Admin' 
+      'admin_devise'
+    else
+      'application'
+    end
+  end
 end
